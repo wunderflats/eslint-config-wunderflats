bf441bf3e24ca91c40fe4b327846e4d10925de73
Maximilian Schmitt <maximilian.schmitt@googlemail.com>
2016-09-09 15:22:48 +0200

    fix eslintrc export again

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
+const ESLINTRC_PATH = path.join(__dirname, '.eslintrc')
 
-module.exports = JSON.parse(fs.readFileSync('./.eslintrc', 'utf-8'))
+module.exports = JSON.parse(fs.readFileSync(ESLINTRC_PATH, 'utf-8'))
