bf0c2f1d7b453e5b77724cfd5799ae8b3b0ae640
Danny Arnold <despair.blue@gmail.com>
2016-10-05 17:32:25 +0200

    ignore strings and template literals for line length (#3)
    * ignore strings and template literals for line length
    
    * use exact dependencies
    * :arrow_up: eslint@3.7.1
    
    * update readme

---
+++ test/fixtures/max-len.ok.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const myModule = require('../test/../test/../test/../test/../test/../test/../test/../test/../index')
+
+myModule.doSomthing(`
+  this is a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery long template string ${myModule.doSomthing()}
+`)
